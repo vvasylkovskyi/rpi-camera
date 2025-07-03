61a87f801ba48655955c859bb2c1dbec8927a6e5
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-03 14:59:27 +0100

    Fix build

--- app/src/rpi_camera/main.py
+++ app/src/rpi_camera/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="RPI Camera API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
