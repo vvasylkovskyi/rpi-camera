3db2d7851f23211d5032a12d083fc1076edd9537
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-23 14:16:01 +0100

    Fix

--- infra/terraform/create-ssl-credentials_video_service_web.sh
+++ infra/terraform/create-ssl-credentials_video_service_web.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 # Read and encode each piece
-CERT=$(terraform output -raw iot_thing_video_service_web_certificate_pem | base64 | tr -d '\n')
-KEY=$(terraform output -raw iot_thing_video_service_web_private_key | base64 | tr -d '\n')
-ROOT_CA=$(curl -sS https://www.amazontrust.com/repository/AmazonRootCA1.pem | base64 | tr -d '\n')
+CERT=$(terraform output -raw iot_thing_video_service_web_certificate_pem | base64 -w 0)
+KEY=$(terraform output -raw iot_thing_video_service_web_private_key | base64 -w 0)
+ROOT_CA=$(curl -sS https://www.amazontrust.com/repository/AmazonRootCA1.pem | base64 -w 0)
 
 # Print as JSON
 cat <<EOF
