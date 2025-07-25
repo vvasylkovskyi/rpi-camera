9d05f81031a7fedeac0c55dc8f17119e4a53227e
vvasylkovskyi <viktor.vasylkovskyi@gmail.com>
2025-07-25 10:36:45 +0100

    Fix

--- rpi-camera-software/src/rpi_camera/models/camera_control_event.py
+++ rpi-camera-software/src/rpi_camera/models/camera_control_event.py
@@ -8,28 +8,15 @@ class CameraAction(Enum):
     START = "start"
     STOP = "stop"
     START_WEBRTC_STREAM = "start_webrtc_stream"
-<<<<<<< HEAD
     START_WEBRTC_STREAM_ANSWER = "start_webrtc_stream_answer"
     STOP_WEBRTC_STREAM = "stop_webrtc_stream"
 
 class WebRTCOffer(BaseModel):
     type: str
     sdp: str
-=======
-    STOP_WEBRTC_STREAM = "stop_webrtc_stream"
-
-
-class WebRTCOffer(BaseModel):
-    type: str
-    sdp: str
-
->>>>>>> f0114d1 (Implemented Web RTC MQTT exchange)
 
 class CameraControlEvent(BaseModel):
     action: CameraAction
     webrtc_offer: Optional[WebRTCOffer] = None
-<<<<<<< HEAD
     webrtc_answer: Optional[str] = None
 
-=======
->>>>>>> f0114d1 (Implemented Web RTC MQTT exchange)
