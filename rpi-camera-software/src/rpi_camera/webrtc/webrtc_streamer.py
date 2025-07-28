c784816563a6bb7f6cc135e11bc338a6efd42b27
vvasylkovskyi <viktor.vasylkovskyi@gmail.com>
2025-07-28 17:36:48 +0100

    Fix

--- rpi-camera-software/src/rpi_camera/webrtc/webrtc_streamer.py
+++ rpi-camera-software/src/rpi_camera/webrtc/webrtc_streamer.py
@@ -2,7 +2,7 @@ import asyncio
 import av
 import fractions
 import time
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from aiortc import RTCConfiguration, RTCIceServer, RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
 from picamera2 import Picamera2
 from rpi_camera.logger.logger import Logger 
 
@@ -29,13 +29,39 @@ class PicameraVideoTrack(VideoStreamTrack):
         self.logger.debug(f"Video frame PTS: {video_frame.pts}, time_base: {video_frame.time_base}")
         return video_frame
 
+ICE_SERVERS = [
+    RTCIceServer(urls="stun:stun.l.google.com:19302"),
+    # Optional: Add TURN server if needed
+    RTCIceServer(
+        urls="turn:standard.relay.metered.ca:80",
+        username="267a8ddc0b92539bebd0229e",
+        credential="Inh/lf0T5gQR7i77"
+    ),
+    RTCIceServer(
+        urls="turn:standard.relay.metered.ca:80?transport=tcp",
+        username="267a8ddc0b92539bebd0229e",
+        credential="Inh/lf0T5gQR7i77"
+    ),
+    RTCIceServer(
+        urls="turn:standard.relay.metered.ca:443",
+        username="267a8ddc0b92539bebd0229e",
+        credential="Inh/lf0T5gQR7i77"
+    ),
+    RTCIceServer(
+        urls="turns:standard.relay.metered.ca:443?transport=tcp",
+        username="267a8ddc0b92539bebd0229e",
+        credential="Inh/lf0T5gQR7i77"
+    )
+]
+
 class WebRTCStreamer:
     def __init__(self):
         self.pc = None
         self.logger = Logger("WebRTCStreamer")
 
     async def start(self, offer_sdp, video_track):
-        self.pc = RTCPeerConnection()
+        rtc_config = RTCConfiguration(ICE_SERVERS)
+        self.pc = RTCPeerConnection(configuration=rtc_config)
         self.pc.addTrack(video_track)
 
         await self.pc.setRemoteDescription(RTCSessionDescription(sdp=offer_sdp, type="offer"))
